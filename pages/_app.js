cb330d837eb4c3e7c9e79129d13609ef03245042
Tobias Norton <tnorton20@yahoo.com>
2020-05-28 18:18:02 -0400

    Creates referral form and dashboard with basic styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import * as ReferralModal from 'react-modal'
+import { Formik, Field } from 'formik'
 // import styles from '../styles/app.module.scss'
 import Navigation from '../components/navigation'
 import Head from 'next/head'
@@ -22,11 +23,140 @@ const App = ({ Component, pageProps }) => {
       </div>
       <ReferralModal isOpen={isModalOpen}>
         <div className="referral-modal-contents">
-          <h2>Refer a friend now and start earning rewards!</h2>
-          <button onClick={() => setIsModalOpen(false)}>Close</button>
+          <button className="close-modal" onClick={() => setIsModalOpen(false)}>
+            X
+          </button>
+          <h2>Refer a friend now and start earning!</h2>
+          <p className="caption">
+            Enter a friend's first name and email and we'll send them an invitation to sign up.
+          </p>
+          <div>
+            {/* Assumes the user is registered and logged in*/}
+            <Formik initialValues={{ friendsEmail: '', firstName: '' }} onSubmit={''}>
+              {({ status, isSubmitting }) => {
+                return (
+                  <form>
+                    <div className="referral-fields-container">
+                      <div>
+                        <label htmlFor="firstName">First Name</label>
+                        <Field
+                          type="text"
+                          name="firstName"
+                          style={{ 'font-size': '14px', height: '18px' }}
+                        ></Field>
+                      </div>
+                      <div>
+                        <label className="email-label" htmlFor="email">
+                          Email
+                        </label>
+                        <Field
+                          type="email"
+                          name="email"
+                          style={{ 'font-size': '14px', height: '18px' }}
+                        ></Field>
+                      </div>
+                    </div>
+                    <button className="submit" type="submit" disabled={isSubmitting}>
+                      Send Invite
+                    </button>
+                    {status && status.error ? <p>{status.error}</p> : null}
+                  </form>
+                )
+              }}
+            </Formik>
+            <div className="rewards-dash">
+              <p className="rewards-dash-head">Rewards Dashboard</p>
+              <table className="referral-list">
+                <tr className="referral-list-head">
+                  <th>
+                    <p className="header-number">10</p>
+                    <p>Friends Referred</p>
+                  </th>
+                  <th>
+                    <p className="header-number">8</p>
+                    <p>Rewards Earned</p>
+                  </th>
+                  <th>
+                    <p className="header-number">4</p>
+                    <p>Rewards Pending</p>
+                  </th>
+                </tr>
+                <tr className="referral">
+                  <td>Hector</td>
+                  <td>
+                    <p>Referred</p>
+                    <p>5 days ago</p>
+                  </td>
+                  <td>Last</td>
+                </tr>
+                <tr className="referral">
+                  <td>Hector</td>
+                  <td>
+                    <p>Referred</p>
+                    <p>5 days ago</p>
+                  </td>
+                  <td>Last</td>
+                </tr>
+                <tr className="referral">
+                  <td>Hector</td>
+                  <td>
+                    <p>
+                      Referred
+                      <br />
+                      <span></span>5 days ago
+                    </p>
+                    {/* <p>5 days ago</p> */}
+                  </td>
+                  <td>Last</td>
+                </tr>
+                <tr className="referral">
+                  <td>Hector</td>
+                  <td>
+                    <p>Referred</p>
+                    <p>5 days ago</p>
+                  </td>
+                  <td>Last</td>
+                </tr>
+                <tr className="referral">
+                  <td>Hector</td>
+                  <td>
+                    <p>Referred</p>
+                    <p>5 days ago</p>
+                  </td>
+                  <td>Last</td>
+                </tr>
+                <tr className="referral">
+                  <td>Hector</td>
+                  <td>
+                    <p>Referred</p>
+                    <p>5 days ago</p>
+                  </td>
+                  <td>Last</td>
+                </tr>
+                <tr className="referral">
+                  <td>Hector</td>
+                  <td>
+                    <p>Referred</p>
+                    <p>5 days ago</p>
+                  </td>
+                  <td>Last</td>
+                </tr>
+                <tr className="referral">
+                  <td>Hector</td>
+                  <td>
+                    <p>Referred</p>
+                    <p>5 days ago</p>
+                  </td>
+                  <td>Last</td>
+                </tr>
+              </table>
+            </div>
+          </div>
         </div>
       </ReferralModal>
-      <button onClick={() => setIsModalOpen(true)}>Click Me</button>
+      <button className="referral-button" onClick={() => setIsModalOpen(true)}>
+        Refer and Earn
+      </button>
 
       <style jsx>{`
         .app {
@@ -35,6 +165,12 @@ const App = ({ Component, pageProps }) => {
           flex-direction: column;
           align-items: center;
           width: full;
+        }
+
+        .referral-button {
+          position: fixed;
+          bottom: 30px;
+          right: 30px;
         }
 
         .referral-modal {
@@ -49,15 +185,101 @@ const App = ({ Component, pageProps }) => {
           // border-radius: 4px;
           // outline: none;
           // padding: 20px;
-          background-color: red;
         }
+
         .referral-modal-contents {
           display: flex;
           flex-direction: column;
           align-items: center;
           width: 94%;
           font-family: 'Poppins', sans-serif;
+          margin: auto;
         }
+
+        .close-modal {
+          display: block;
+          position: fixed;
+          right: 45px;
+          margin-right: 20px;
+          z-index: 100;
+        }
+
+        h2 {
+          margin-top: 100px;
+          text-align: center;
+        }
+
+        .caption {
+          margin: 0 0 40px 0;
+          text-align: center;
+        }
+        
+        form {
+          font-family: 'Poppins', sans-serif;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          flex-wrap: wrap;
+          max-width: 800px;
+        }
+        
+        .referral-fields-container {
+          display: flex;
+          flex-direction: column
+          align-items: center;
+        }
+        
+        label {
+          margin-right: 10px;
+        }
+        
+        .email-label {
+          margin-left: 20px;
+        }
+        
+        .submit {
+          font-family: 'Poppins', sans-serif;
+          font-size: 16px;
+          padding: 10px;
+          margin: 20px 0;
+          width: 100%;
+          border: none;
+          background-color: #00bfe2;
+          color: white;
+        }
+
+        .rewards-dash-head {
+          font-size: 20px;
+          font-weight: bold;
+          text-align: center;
+          padding-top: 20px;
+          margin-top: 0;
+          border-top: 1px solid hsl(191, 0%, 80%);
+        }
+
+        .referral-list-head p {
+          margin-top: 0;
+        }
+
+        table {
+          border: 1px solid light-grey;
+          width: 100%;
+        }
+
+        .header-number {
+          color: #00bfe2;
+          font-size: 50px;
+          margin: 0;
+        }
+
+        .referral p {
+          margin: 0;
+        }
+
+        .referral p span {
+          font-size: 10px;
+        }
+
       `}</style>
     </div>
   )
